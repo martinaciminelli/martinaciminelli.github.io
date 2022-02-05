f652db392c4bca6ca900048e7e1d98ae0dcc15cf
Gustavo Nicoletti Rosa <nicolettigu@gmail.com>
2022-02-06 00:19:00 +0100

    App Android, cringeometro a pulsante basico

---
+++ app/cringeometro/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cringeometro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Cringeometro'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool cringe = false;
+  void cringeDetected() {
+    setState(() {
+      cringe = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by theD method above.
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: <Widget>[
+            cringe ? const Text('CRINGE DETECTED'): const Text('everything is normal'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                  height: 200.0,
+                  width: 200.0,
+                  child: FittedBox(
+                    child: FloatingActionButton(
+                onPressed:cringeDetected,
+                tooltip: 'detects cringe',
+                child: const CircleAvatar(
+                  radius: 48, // Image radius
+                  backgroundImage: NetworkImage("https://i.ibb.co/FBb2B1G/foto-martina.png"),
+          )
+        ),
+        ),
+       
+      ),)
+        
+        
+        ]
+          
+      ),
+      
+      
+      ),
+    );
+  }
+}
